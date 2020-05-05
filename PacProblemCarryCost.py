4c039d1b153b04200fd8a30c09159a16f472016b
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-05-05 20:28:24 -0300

    Adapted SA and create a new problem class which allows heuristics based on the path's score

---
+++ PacProblemCarryCost.py
@@ -0,0 +1,112 @@
+'''
+Project 1 - Search-based solutions for static Pac-Man game.
+
+PacProblem.py: Basic problem type for Pac-Man problem.
+
+Subject:
+    MC906/MO416 - Introduction to Artificial Intelligence.
+Authors:
+    Daniel Helú Prestes de Oliveira - RA 166215
+    Eduardo Barros Innarelli        - RA 170161
+    Matheus Rotta Alves             - RA 184403
+    Victor Ferreira Ferrari         - RA 187890
+    Vinícius Couto Espindola        - RA 188115
+
+University of Campinas - UNICAMP - 2020
+
+Last Modified: 03/05/2020.
+'''
+
+# Add aima folder to PYTHONPATH environment variable.
+from search import Problem
+from numpy import sqrt
+
+class PacProblem(Problem):
+    ''' Modeling the static Pac-Man game problem for search. '''
+    
+    def __init__(self, initial, goal, maze):
+        ''' Initial State:
+            Tuple of 2 elements. (i,j) in maze.
+            Goal State:
+            Tuple of 2 elements. (i,j) in maze.
+            Maze:
+            NumPy array of BYTES (to save RAM)
+        '''
+        Problem.__init__(self, initial, goal)
+        self.maze = maze
+        
+    def actions(self, state):
+        ''' A state is the index of the maze (tuple). 
+            An action is a tuple of i,j with the direction to walk.
+        '''
+        self.maze[state[0]] = ' ' # Eat dot
+
+        actions = []
+        possible = [(1,0),(-1,0),(0,1),(0,-1)]
+        idx,_ = state
+        for action in possible:
+            nxt = list(map(sum, zip(idx,action)))
+            
+            # Check circling around maze. If < 0, negative indexing will do the job.
+            if nxt[0] == self.maze.shape[0]:
+                nxt[0] = 0
+            elif nxt[1] == self.maze.shape[1]:
+                nxt[1] = 0
+            nxt = tuple(nxt)
+            
+            # Check ghosts and walls.
+            if self.maze[nxt] not in [b'o', b'|', b'-']:
+                actions.append(action)
+        return actions
+
+    def goal_test(self, state):
+        ''' Check if the Pac-Man reaches its destination.'''
+        return state[0] == self.goal
+
+    def result(self, state, action):
+        ''' The result of an action is to move to the next position, and eat the point if needed.'''
+        idx,cost = state
+        
+        # Get next position.
+        nxt = list(map(sum, zip(idx,action)))
+        
+        # Circle around maze.
+        if nxt[0] == self.maze.shape[0]:
+            nxt[0] = 0
+        elif nxt[0] < 0:
+            nxt[0] = self.maze.shape[0]-1
+        elif nxt[1] == self.maze.shape[1]:
+            nxt[1] = 0
+        elif nxt[1] < 0:
+            nxt[1] = self.maze.shape[1]-1
+        
+        nxt = tuple(nxt)
+        nxt_cost = self.path_cost(cost, None, None, (nxt,))
+        return (nxt,nxt_cost)
+    
+    def path_cost(self, c, state1, action, state2):
+        ''' 10 points if it eats a point, and minus 1 point per movement. '''
+        # Goal is same as a point (for now)
+        nxt = self.maze[state2[0]]
+        if nxt == b'.' or nxt == b'?':
+            cost = c-10
+        else:
+            cost = c
+        return cost+1
+
+    def value(self, state):
+        ''' Value is the "score" for the state.'''
+        idx,cost = state
+
+        # Use the score as a heuristic
+        # return cost
+        
+        # Use euclidean distance as a heuristic
+        Ax,Ay = idx
+        Bx,By = self.goal
+        return -10*sqrt((Ax-Bx)**2 + (Ay-By)**2)
+
+        # Use manhatam distance as a heuristic
+        # Ax,Ay = idx
+        # Bx,By = self.goal
+        # return -10*(np.abs(Ax-Bx) + np.abs(Ay-By))
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -26,117 +26,138 @@ sys.path.insert(0,f'{dir}/aima-python')
 import numpy as np
 from search import Problem
 from search import simulated_annealing_full
+# from PacProblemNoMaze import PacProblem
+from PacProblemCarryCost import PacProblem
 
-
-class PacProblem(Problem):
-    ''' Modeling the static Pac-Man game problem for search. '''
+# class PacProblem(Problem):
+#     ''' Modeling the static Pac-Man game problem for search. '''
     
-    def __init__(self, initial, goal, maze):
-        ''' Initial State:
-            Tuple of 2 elements. (i,j) in maze.
-            Goal State:
-            Tuple of 2 elements. (i,j) in maze.
-            Maze:
-            NumPy array of BYTES (to save RAM)
-            flags (sa):
-            dictionary with the 
-        '''
-        Problem.__init__(self, initial, goal)
-        self.maze = maze
+#     def __init__(self, initial, goal, maze):
+#         ''' Initial State:
+#             Tuple of 2 elements. (i,j) in maze.
+#             Goal State:
+#             Tuple of 2 elements. (i,j) in maze.
+#             Maze:
+#             NumPy array of BYTES (to save RAM)
+#             flags (sa):
+#             dictionary with the 
+#         '''
+#         Problem.__init__(self, initial, goal)
+#         self.maze = maze
 
-    def actions(self, state):
-        ''' A state is the index of the maze (tuple). 
-            An action is a tuple of i,j with the direction to walk.
-        '''
-        self.maze[state] = ' ' # Eat dot
+#     def actions(self, state):
+#         ''' A state is the index of the maze (tuple). 
+#             An action is a tuple of i,j with the direction to walk.
+#         '''
+#         self.maze[state] = ' ' # Eat dot
 
-        actions = []
-        possible = [(1,0),(-1,0),(0,1),(0,-1)]
-        idx = state
-        for action in possible:
-            nxt = list(map(sum, zip(idx,action)))
+#         actions = []
+#         possible = [(1,0),(-1,0),(0,1),(0,-1)]
+#         idx = state
+#         for action in possible:
+#             nxt = list(map(sum, zip(idx,action)))
             
-            # Check circling around maze. If < 0, negative indexing will do the job.
-            if nxt[0] == self.maze.shape[0]:
-                nxt[0] = 0
-            elif nxt[1] == self.maze.shape[1]:
-                nxt[1] = 0
-            nxt = tuple(nxt)
+#             # Check circling around maze. If < 0, negative indexing will do the job.
+#             if nxt[0] == self.maze.shape[0]:
+#                 nxt[0] = 0
+#             elif nxt[1] == self.maze.shape[1]:
+#                 nxt[1] = 0
+#             nxt = tuple(nxt)
             
-            # Check ghosts and walls.
-            if self.maze[nxt] not in [b'o', b'|', b'-']:
-                actions.append(action)
-        return actions
+#             # Check ghosts and walls.
+#             if self.maze[nxt] not in [b'o', b'|', b'-']:
+#                 actions.append(action)
+#         return actions
 
-    def goal_test(self, state):
-        ''' Check if the Pac-Man reaches its destination.'''
-        return state == self.goal
+#     def goal_test(self, state):
+#         ''' Check if the Pac-Man reaches its destination.'''
+#         return state == self.goal
 
-    def result(self, state, action):
-        ''' The result of an action is to move to the next position, and eat the point if needed.'''
-        idx = state
+#     def result(self, state, action):
+#         ''' The result of an action is to move to the next position, and eat the point if needed.'''
+#         idx = state
         
-        # Get next position.
-        nxt = list(map(sum, zip(idx,action)))
+#         # Get next position.
+#         nxt = list(map(sum, zip(idx,action)))
         
-        # Circle around maze. If < 0, negative indexing will do the job.
-        if nxt[0] == self.maze.shape[0]:
-            nxt[0] = 0
-        elif nxt[0] < 0:
-            nxt[0] = self.maze.shape[0]-1
-        elif nxt[1] == self.maze.shape[1]:
-            nxt[1] = 0
-        elif nxt[1] < 0:
-            nxt[1] = self.maze.shape[1]-1
+#         # Circle around maze. If < 0, negative indexing will do the job.
+#         if nxt[0] == self.maze.shape[0]:
+#             nxt[0] = 0
+#         elif nxt[0] < 0:
+#             nxt[0] = self.maze.shape[0]-1
+#         elif nxt[1] == self.maze.shape[1]:
+#             nxt[1] = 0
+#         elif nxt[1] < 0:
+#             nxt[1] = self.maze.shape[1]-1
         
-        nxt = tuple(nxt)
-        return nxt
+#         nxt = tuple(nxt)
+#         return nxt
     
-    def path_cost(self, c, state1, action, state2):
-        ''' 10 points if it eats a point, and minus 1 point per movement. '''
-        if self.maze[state2] == b'.':
-            cost = c-10
-        else:
-            cost = c
-        return cost+1
+#     def path_cost(self, c, state1, action, state2):
+#         ''' 10 points if it eats a point, and minus 1 point per movement. '''
+#         if self.maze[state2] == b'.':
+#             cost = c-10
+#         else:
+#             cost = c
+#         return cost+1
 
-    # TODO: Supervalue to goal and parameters of the cooling function
-    def value(self, state):
-        ''' Value is the "score" for the state.'''
-        # Use the score as a heuristic
-        # if self.maze[state] == b'.': return 10
-        # else: return 0
+#     # TODO: Supervalue to goal and parameters of the cooling function
+#     def value(self, state):
+#         ''' Value is the "score" for the state.'''
+#         # Use the score as a heuristic
+#         # if self.maze[state] == b'.': return 10
+#         # else: return 0
         
-        # Use euclidean distance as a heuristic
-        Ax,Ay = state
-        Bx,By = self.goal
-        return -10*np.sqrt((Ax-Bx)**2 + (Ay-By)**2)
+#         # Use euclidean distance as a heuristic
+#         Ax,Ay = state
+#         Bx,By = self.goal
+#         return -10*np.sqrt((Ax-Bx)**2 + (Ay-By)**2)
 
-        # Use manhatam distance as a heuristic
-        # Ax,Ay = state
-        # Bx,By = self.goal
-        # return -10*(np.abs(Ax-Bx) + np.abs(Ay-By))
+#         # Use manhatam distance as a heuristic
+#         # Ax,Ay = state
+#         # Bx,By = self.goal
+#         # return -10*(np.abs(Ax-Bx) + np.abs(Ay-By))
 
-def get_best_path(path, maze, goal):
+# def get_best_path(path, maze, goal):
+#         ''' Calculate the cost after getting a path from some search method. '''
+#         min_idx = None 
+#         min_cost = sys.maxsize
+
+#         cost = -1
+#         for i,p in enumerate(path):        
+#             if maze[p] == b'.':
+#                 maze[p] = ' ' 
+#                 cost -= 10
+#             cost += 1 # Pay to move
+
+#             # Check if at a goal state with a better cost
+#             if p == goal and cost < min_cost:
+#                 min_cost = cost
+#                 min_idx = i
+
+#         # Check it reached the goal state at all
+#         if min_idx : return (path[:min_idx+1], min_cost)
+#         else : None
+
+# NOTE: It assumes the state contains it's respective score
+def get_best_path(states, maze, goal):
         ''' Calculate the cost after getting a path from some search method. '''
         min_idx = None 
         min_cost = sys.maxsize
 
-        cost = -1
-        for i,p in enumerate(path):        
-            if maze[p] == b'.':
-                maze[p] = ' ' 
-                cost -= 10
-            cost += 1 # Pay to move
-
+        for i,(p,c) in enumerate(states):        
             # Check if at a goal state with a better cost
-            if p == goal and cost < min_cost:
-                min_cost = cost
+            if p == goal and c < min_cost:
+                min_cost = c
                 min_idx = i
 
         # Check it reached the goal state at all
-        if min_idx : return (path[:min_idx+1], min_cost)
-        else : None
+        if min_idx: 
+            path = [states[i][0] for i in range(min_idx+1)]
+            return True, (path, min_cost)
+        else: 
+            path = [s[0] for s in states]
+            return False, (path, None)
 
 def cooling_func(k=10, lam=0.001, limit=2000):
     """One possible schedule function for simulated annealing"""
@@ -148,32 +169,35 @@ def sa_pacman(maze, fill=False, wrong_path=True):
 
     # Get and clear positions
     init = tuple(map(int, np.where(maze==b'!')))
-    maze[init] = b' '
     goal = tuple(map(int, np.where(maze==b'?'))) 
+    maze[init] = b' '
     maze[goal] = b' '
 
     # Copy for post processing
     maze_ref = maze.copy() 
     
     # Create Problem
-    prob = PacProblem(init, goal, maze)
+    prob = PacProblem((init,0), goal, maze) # Add initial cost 0 with init
     assert maze[goal] == b'.' or maze[goal] == b' ', "Goal unreachable."
     
     # Solve with Simulated Annealing
     states = simulated_annealing_full(prob, schedule=cooling_func())
 
     # Get the best path found
-    best = get_best_path(states, maze_ref, goal)
-    if not best: print("The search failed to reach a final state.")
+    reached,best = get_best_path(states, maze_ref, goal)
+    if not reached: print("The search failed to reach a final state.")
     
     # If goal reached return best
-    if best:
+    if reached:
         return best
+    # If wrong path allowd, return it since did not reach
     elif wrong_path:    
-        return (states,sys.maxsize)
+        return best
 
 if __name__ == '__main__':
-    best = sa_pacman('mazes/tiny/3')
+    maze_file = 'mazes/dense/1a'
+    maze = np.genfromtxt(maze_file, dtype=str, delimiter=1).astype('bytes')
+    best = sa_pacman(maze)
     if best:
         print('\nAcquired Path: \n', best)
     else:
